f70d5845b386e79620b1892fd161c1df9b5e113b
Pueropstar <majesoung@naver.com>
2023-07-04 21:48:51 +0900

    feat: installed app 추가

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'petsitters',
     'customers',
     'rest_framework',
+    'rest_framework.authtoken'
 
 ]
 
@@ -128,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.Member'
