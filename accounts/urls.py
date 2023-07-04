54f3d6b11d9d9ac75db5577ceb12eb57109afe3e
Pueropstar <majesoung@naver.com>
2023-07-04 21:48:36 +0900

    feat: url 라우팅

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import SignupView, LoginView
+
+urlpatterns=[
+    path('signup/',SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+]
